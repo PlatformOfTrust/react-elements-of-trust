d565e9eb57a57e394828f930346ffd61fcb6aed8
Dennis Kumar <dennis@lieturd.com>
2019-08-05 15:12:49 +0300

    Flow typed.

--- src/Components/FormGroup/component.js
+++ src/Components/FormGroup/component.js
@@ -2,17 +2,30 @@ import React, { Component } from 'react';
 
 import Form from 'react-bootstrap/Form';
 
-class FormGroup extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            controlId: this.props.controlId,
-            muted: this.props.muted,
-            label: this.props.label,
-            type: this.props.type,
-            placeholder: this.props.placeholder
-        };
-    }
+type FormProps = {
+    controlId: String,
+    muted?: string,
+    label: String,
+    type: String,
+    placeholder?: string
+};
+
+type FormState = {
+    controlId: String,
+    muted?: string,
+    label: String,
+    type: String,
+    placeholder?: string
+};
+
+class FormGroup extends Component<FormProps, FormState> {
+    state = {
+        controlId: this.props.controlId,
+        muted: this.props.muted,
+        label: this.props.label,
+        type: this.props.type,
+        placeholder: this.props.placeholder
+    };
 
     render() {
         return (
