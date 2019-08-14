4769e70e233378f0b195c56683913ddc5efc7584
Dennis Kumar <dennis@lieturd.com>
2019-08-14 13:36:37 +0300

    Removed type props from Link.

--- src/Components/Link/component.js
+++ src/Components/Link/component.js
@@ -1,13 +1,5 @@
 import React, { Component } from 'react';
 
-type LinkProps = {
-    label: string,
-    linkTo: string
-};
-
-type LinkState = {
-    target: string
-};
 
 class Links extends Component<LinkProps, LinkState> {
     render() {
