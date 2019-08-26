3ed1ba431f3b54dce5cbfac4d9af1eb17bf28ece
Dennis Kumar <dennis@lieturd.com>
2019-08-26 14:02:46 +0300

    Run build fail in main.js possibly. testing

--- src/main.js
+++ src/main.js
@@ -2,13 +2,3 @@ export { default as Button } from './Components/Button/component';
 export { default as Tab } from './Components/Tab/component';
 export { default as Tabs } from './Components/Tabs/component';
 export { default as Image } from './Components/Image/component';
-export { default as Breadcrumbs } from './Components/Breadcrumbs/component';
-export {
-    default as ExternalNavBar
-} from './Components/ExternalNavBar/NavBar/component';
-export {
-    default as MainNavBar
-} from './Components/MainNavBar/NavBar/component';
-export { default as FormGroup } from './Components/FormGroup/component';
-export { default as HexImage } from './Components/HexImage/component';
-export { default as Links } from './Components/Link/component';
